aeb920aeb6c5510cbecfbd73c8fbb29677eca15c
Adrian <adrian.gabriel.dev@gmail.com>
2024-02-26 17:58:50 -0300

    commit:: relacionamento com coleções

---
+++ src/br/com/collections/TestaCurso.java
@@ -0,0 +1,17 @@
+package br.com.collections;
+
+import java.util.List;
+
+public class TestaCurso {
+    public static void main(String[] args) {
+        Curso javaCollections =  new Curso("Dominando Coleções no Java", "João Castro");
+        List<Aula> aulas = javaCollections.getAulas();
+
+        //javaCollections.getAulas().add(new Aula("Trabalahndo com Arraylist", 21));
+        javaCollections.addAula(new Aula("Trabalahndo com Arraylist", 21));
+        javaCollections.addAula(new Aula("Criando uma Aula", 20));
+        javaCollections.addAula(new Aula("Modelando Coleções", 23));
+
+        System.out.println(javaCollections.getAulas());
+    }
+}
